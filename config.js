ccc81665b03f4ef36fcfa7939ca91a8429fd39b2
Jason Rush <jasonrush@macbookair.lan>
2025-03-08 15:22:50 -0800

    Remove config.js from tracking

--- config.js
+++
@@ -1,7 +0,0 @@
-// Local configuration - Do not commit this file
-const config = {
-    HUGGING_FACE_API_KEY: 'YOUR_API_KEY_HERE', // Add your key here
-    API_URL: 'https://api-inference.huggingface.co/models/google/flan-t5-small'
-};
-
-export default config; 
