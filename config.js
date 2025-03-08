fc6760d0d3815193ec80143b0ac55b604fb1ab64
Jason Rush <jasonrush@macbookair.lan>
2025-03-08 15:18:04 -0800

    initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+// Local configuration - Do not commit this file
+const config = {
+    HUGGING_FACE_API_KEY: 'YOUR_API_KEY_HERE', // Add your key here
+    API_URL: 'https://api-inference.huggingface.co/models/google/flan-t5-small'
+};
+
+export default config; 
